4d77a2bf2e2b8e4825efb37fb1bc8496d432e521
Adewole <wolecharles@gmail.com>
2023-08-20 18:37:47 +0100

    Feat : validation logic enhanced TIC-6

--- controllers/showcontroller.go
+++ controllers/showcontroller.go
@@ -3,18 +3,20 @@ package controllers
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
 	db "github.com/Wolechacho/ticketmaster-backend/database"
 	"github.com/Wolechacho/ticketmaster-backend/database/entities"
 	sequentialguid "github.com/Wolechacho/ticketmaster-backend/helpers"
+	paginate "github.com/Wolechacho/ticketmaster-backend/helpers/pagination"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
 
 const (
-	TIME_OVERLAP_ERROR = "Time overlap between the show Start and End Time"
+	TIME_OVERLAP_ERROR = "time overlap between the show Start and End Time"
 	INVALID_UUID_ERROR = "%s should have a valid UUID"
 )
 
@@ -23,20 +25,25 @@ type ShowController struct {
 
 func (showController *ShowController) CreateShow(showContext echo.Context) error {
 	var err error
-	request := new(createMovieRequest)
+	request := new(createShowRequest)
 	err = showContext.Bind(request)
-
+	fmt.Println(request)
 	if err != nil {
 		fmt.Println(err)
 		return showContext.JSON(http.StatusBadRequest, err.Error())
 	}
 
-	validationErrors := validateShow(*request)
-	if len(validationErrors) != 0 {
-		return showContext.JSON(http.StatusBadRequest, validationErrors)
+	fieldErrors := validateRequiredFields(*request)
+	if len(fieldErrors) != 0 {
+		return showContext.JSON(http.StatusBadRequest, fieldErrors)
 	}
 
-	response := new(createMovieResponse)
+	showTimeErrors := validateShowTime(*request)
+	if len(showTimeErrors) != 0 {
+		return showContext.JSON(http.StatusBadRequest, showTimeErrors)
+	}
+
+	response := new(createShowResponse)
 
 	err = db.DB.Transaction(func(tx *gorm.DB) error {
 		for _, showTime := range request.ShowTimes {
@@ -70,7 +77,7 @@ func (showController *ShowController) CreateShow(showContext echo.Context) error
 	return showContext.JSON(http.StatusOK, response)
 }
 
-type createMovieRequest struct {
+type createShowRequest struct {
 	ShowTimes    []ShowDateTime `json:"showTimes"`
 	CinemaHallId string         `json:"cinemaHallId"`
 	MovieId      string         `json:"movieId"`
@@ -80,52 +87,175 @@ type ShowDateTime struct {
 	EndDateTime   time.Time `json:"endDate"`
 }
 
-type createMovieResponse struct {
+type createShowResponse struct {
 	ShowIds []string `json:"showIds"`
 }
 
-func validateShow(request createMovieRequest) []string {
-	errors := []string{}
+func (showController ShowController) GetShowsByUserLocation(showContext echo.Context) error {
+	request := new(GetShowsByLocationRequest)
 
+	//get show that are not deprecated nor cancelled
+	//sort them by the earliest show date and time
+
+	// use the cityId to get the : UserLocation -> cinema -> cinemalHall -> Show -> Movie
+
+	//rows, err := db.DB.Table("userLocations").Select("userLocations.CityId").Joins("join cinemas on userLocations.CityId = cinemas.CityId").Joins("join cinemaHalls on cinemas.Id = cinemaHalls.CinemaId").Joins("join shows on cinemaHalls.Id = shows.CinemaHallId").Select()
+	//.Rows()
+
+	//db.Model(&User{}).Select("users.name, emails.email").Joins("left join emails on emails.user_id = users.id").Scan(&result{})
+
+	// rows, err := db.DB.Table("userLocations").
+	// 	Joins("join cinemas on userLocations.CityId = cinemas.CityId").
+	// 	Joins("join cinemaHalls on cinemas.Id = cinemaHalls.CinemaId").
+	// 	Joins("join shows on cinemaHalls.Id = shows.CinemaHallId").
+	// 	Joins("join movies on shows.MovieId = movies.Id").
+	// 	Where("userLocations.UserId = ? AND cinemas.IsDeprecated = ? AND cinemaHalls.IsDeprecated = ? AND shows.IsDeprecated = ? AND movies.IsDeprecated = ?", request.UserId, false, false, false, false).
+	// 	Select("shows.Id,shows.Date,shows.StartTime,shows.EndTime,movies.Id,movies.Title,movies.Description,movies.Language,movies.Genre,userLocations.CityId,cinemas.Id").
+	// 	Rows()
+
+	//filter
+	filterClause := query(*request)
+
+	//paginate
+	paginateClause := paginate.Paginate(request.Page, request.PageLength)
+
+	//orderby
+	sortandorder := fmt.Sprintf("%s %s", request.SortBy, request.Order)
+	orderByClause := paginate.OrderBy(sortandorder)
+
+	rows, err := db.DB.Scopes(filterClause, paginateClause, orderByClause).Rows()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	shows := []GetShowsByLocationDTO{}
+	for rows.Next() {
+		var show GetShowsByLocationDTO
+		err = rows.Scan(&show)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+		shows = append(shows, show)
+	}
+
+	response := new(GetShowsByLocationResponse)
+	response.Results = shows
+
+	return showContext.JSON(http.StatusOK, response)
+}
+
+type GetShowsByLocationRequest struct {
+	UserId     string `json:"userId"`
+	Page       int    `query:"page"`
+	PageLength int    `query:"pageLength"`
+	SortBy     string `query:"sortBy"`
+	Order      string `query:"order"`
+}
+
+type GetShowsByLocationResponse struct {
+	Results []GetShowsByLocationDTO
+}
+
+type GetShowsByLocationDTO struct {
+	Id          string    `json:"id"`
+	Date        time.Time `json:"showDate"`
+	StartTime   int64     `json:"showStartTime"`
+	EndTime     int64     `json:"showEndTime"`
+	MovieId     string    `json:"movieId"`
+	Title       string    `json:"movieTitle"`
+	Description string    `json:"movieDescription"`
+	Language    string    `json:"language"`
+	Genre       int       `json:"genre"`
+}
+
+func query(filter GetShowsByLocationRequest) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Table("userLocations").
+			Joins("join cinemas on userLocations.CityId = cinemas.CityId").
+			Joins("join cinemaHalls on cinemas.Id = cinemaHalls.CinemaId").
+			Joins("join shows on cinemaHalls.Id = shows.CinemaHallId").
+			Joins("join movies on shows.MovieId = movies.Id").
+			Where("userLocations.UserId = ? AND cinemas.IsDeprecated = ? AND cinemaHalls.IsDeprecated = ? AND shows.IsDeprecated = ? AND shows.IsCancelled = ? AND movies.IsDeprecated = ?",
+				filter.UserId, false, false, false, false, false).
+			Select("shows.Id,shows.Date,shows.StartTime,shows.EndTime,movies.Id,movies.Title,movies.Description,movies.Language,movies.Genre,userLocations.CityId,cinemas.Id")
+	}
+}
+
+func validateRequiredFields(request createShowRequest) []error {
+	var validationErrors []error
 	defaultTime, _ := time.Parse("2006-01-02", entities.DEFAULT_TIME)
-	today := time.Now().Local()
-	var minShowTime float64 = 1
-	var maxShowTime float64 = 4
 
-	var timeOverlap = false
-	var tempStartDate = today
-	var tempEndDate = today
+	//validate the cinemaHallId and movieId
+	if len(request.CinemaHallId) == 0 || len(request.CinemaHallId) < 36 {
+		validationErrors = append(validationErrors, fmt.Errorf("cinemaHallId is a required field  with 36 characters"))
+	}
+
+	if request.CinemaHallId == entities.DEFAULT_UUID {
+		validationErrors = append(validationErrors, fmt.Errorf("cinemaHallId should have a valid UUID"))
+	}
+
+	if len(request.MovieId) == 0 || len(request.MovieId) < 36 {
+		validationErrors = append(validationErrors, fmt.Errorf("movieId is a required field with 36 characters"))
+	}
+
+	if request.MovieId == entities.DEFAULT_UUID {
+		validationErrors = append(validationErrors, fmt.Errorf("movieId should have a valid UUID"))
+	}
+
+	if len(request.ShowTimes) == 0 {
+		validationErrors = append(validationErrors, fmt.Errorf("show times is empty"))
+	}
 
 	//Validate the show time
 	for i, showTime := range request.ShowTimes {
 		if showTime.StartDateTime == defaultTime {
-			errors = append(errors, "startDate is a required field")
+			validationErrors = append(validationErrors, fmt.Errorf("showTimes[%d].startDate is a required field", i))
 		}
 
 		if showTime.EndDateTime == defaultTime {
-			errors = append(errors, "endDate is a required field")
+			validationErrors = append(validationErrors, fmt.Errorf("showTimes[%d].endDate is a required field", i))
 		}
+	}
+	return validationErrors
+}
 
+func validateShowTime(request createShowRequest) []error {
+
+	var minShowTime float64 = 1
+	var maxShowTime float64 = 4
+	var validationErrors []error
+
+	defaultTime, _ := time.Parse("2006-01-02", entities.DEFAULT_TIME)
+	today := time.Now().Local()
+
+	timeOverlap := false
+	tempStartDate := today
+	tempEndDate := today
+
+	//Validate the show time
+	for i, showTime := range request.ShowTimes {
 		if showTime.StartDateTime != defaultTime && showTime.StartDateTime.Before(today) {
-			errors = append(errors, "show startdate can be added for future dates")
+			validationErrors = append(validationErrors, fmt.Errorf("showTimes[%d].startDate can only be added for future dates", i))
 		}
 
 		if showTime.EndDateTime != defaultTime && showTime.EndDateTime.Before(today) {
-			errors = append(errors, "show enddate can be added for future dates")
+			validationErrors = append(validationErrors, fmt.Errorf("showTimes[%d].endDate can only be added for future dates", i))
 		}
 
 		if showTime.StartDateTime.Equal(showTime.EndDateTime) {
-			errors = append(errors, "show cannot start and end at the same time")
+			validationErrors = append(validationErrors, fmt.Errorf("showTimes[%d].startDate and showTimes[%d].endDate cannot start and end at the same time", i, i))
 		}
 
 		if showTime.StartDateTime.After(showTime.EndDateTime) {
-			errorMessage := fmt.Sprintf("show time end time %s must be greater than the start time %s", showTime.EndDateTime.Format("2006-01-02T15:04:05"), showTime.StartDateTime.Format("2006-01-02T15:04:05"))
-			errors = append(errors, errorMessage)
+			errorMessage := fmt.Sprintf("showTimes[%d].endDate: %s must be greater than showTimes[%d].startDate: %s", i, showTime.EndDateTime.Format("2006-01-02T15:04:05"), i, showTime.StartDateTime.Format("2006-01-02T15:04:05"))
+			validationErrors = append(validationErrors, fmt.Errorf(errorMessage))
 		}
 
 		hours := showTime.EndDateTime.Sub(showTime.StartDateTime).Hours()
 		if hours < minShowTime || hours > maxShowTime {
-			errors = append(errors, fmt.Sprintf("Show time must be between %.0fhrs and %.0fhrs", minShowTime, maxShowTime))
+			validationErrors = append(validationErrors, fmt.Errorf("showTimes[%d] must be between %.0fhrs and %.0fhrs", i, minShowTime, maxShowTime))
 		}
 
 		//check for date overlap
@@ -142,56 +272,8 @@ func validateShow(request createMovieRequest) []string {
 
 	//if there is overlap, add error
 	if timeOverlap {
-		errors = append(errors, TIME_OVERLAP_ERROR)
+		validationErrors = append(validationErrors, fmt.Errorf(TIME_OVERLAP_ERROR))
 	}
 
-	//validate the cinemaHallId and movieId
-	if len(request.CinemaHallId) == 0 || len(request.CinemaHallId) < 36 {
-		errors = append(errors, "cinemaHallId is a required field  with 36 characters")
-	}
-
-	if request.CinemaHallId == entities.DEFAULT_UUID {
-		errors = append(errors, "cinemaHallId should have a valid UUID")
-	}
-
-	if len(request.MovieId) == 0 || len(request.MovieId) < 36 {
-		errors = append(errors, "movieId is a required field with 36 characters")
-	}
-
-	if request.MovieId == entities.DEFAULT_UUID {
-		errors = append(errors, "movieId should have a valid UUID")
-	}
-
-	return errors
-}
-
-func (showController ShowController) GetShowsByUserLocation(showContext echo.Context) error {
-	//get show that are not deprecated nor cancelled
-	//sort them by the earlist show date and time
-	// UserId -> User -> CityId -> Cinema ->
-
-	// use the cityId to get the : UserLocation -> cinema -> cinemalHall -> Show -> Movie
-
-	return nil
-}
-
-type GetShowsRequest struct {
-	UserId     string `json:"userId"`
-	Page       int    `query:"page"`
-	PageLength int    `query:"pageLength"`
-	SortBy     string `query:"sortBy"`
-	Order      string `query:"order"`
-}
-
-type GetShowsResponse struct {
-	Id           string    `json:"id"`
-	Date         time.Time `json:"showDate"`
-	StartTime    int64     `json:"showStartTime"`
-	EndTime      int64     `json:"showEndTime"`
-	MovieId      string    `json:"movieId"`
-	Title        string    `json:"movieTitle"`
-	Description  string    `json:"movieDescription"`
-	Language     string    `json:"language"`
-	Genre        int       `json:"genre"`
-	IsDeprecated bool
+	return validationErrors
 }
