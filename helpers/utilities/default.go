328a329761f99b9481fdce92e2e0380fd6d57d70
Adewole <wolecharles@gmail.com>
2023-12-03 22:37:37 +0100

    generic implementation for default type

---
+++ helpers/utilities/default.go
@@ -0,0 +1,42 @@
+package utilities
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+func SetDefaults[T any](t *T) error {
+	tType := reflect.TypeOf(*t)
+	if tType.Kind() != reflect.Struct {
+		fmt.Println(tType.Kind())
+		return errors.New("default allowed only allowed on struct")
+	}
+
+	p := reflect.ValueOf(t).Elem()
+
+	for i := 0; i < tType.NumField(); i++ {
+		field := tType.Field(i)
+		if value, ok := field.Tag.Lookup("default"); ok {
+			switch field.Type.Kind() {
+			case reflect.String:
+				if p.Field(i).String() == "" {
+					p.Field(i).SetString(value)
+				}
+
+			case reflect.Int:
+				if p.Field(i).Int() == 0 {
+					if intValue, err := strconv.Atoi(value); err == nil {
+						if !p.Field(i).OverflowInt(int64(intValue)) {
+							p.Field(i).SetInt(int64(intValue))
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
